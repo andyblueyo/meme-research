ceee3e3631ef7a79a921989be73384b88d3e9708
andyblueyo <abchen@uw.edu>
2017-12-08 21:38:30 -0800

    add slider

--- app.R
+++ app.R
@@ -8,9 +8,14 @@ ucMemes <- read.csv("data/ucberkeleymemes_facebook_statuses.csv", stringsAsFacto
 
 
 server <- function(input, output) {
-
+  
 
   output$memePlot <- renderPlotly({
+    maxYVal <- paste0(input$memeYvar, ">=", input$memeReactionSlide[1])
+    minYVal <- paste0(input$memeYvar, "<=", input$memeReactionSlide[2])
+    
+    memeData <- get(input$memeFile) %>% filter_(maxYVal) %>% filter_(minYVal)
+    
     x <- list(
       title = paste(input$memeXvar, " "),
       tickangle = 45,
@@ -25,7 +30,7 @@ server <- function(input, output) {
       t = 50
     )
     
-    plot_ly(get(input$memeFile), x = ~get(input$memeXvar), y = ~get(input$memeYvar), type = "scatter", mode = "markers") %>% 
+    plot_ly(memeData, x = ~get(input$memeXvar), y = ~get(input$memeYvar), type = "scatter", mode = "markers") %>% 
       layout(xaxis = x, yaxis = y, title = paste("Memes from ", input$memeFile), margin = m)
     
   })
@@ -38,8 +43,8 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("memeFile", label = ("Select facebook meme group"), 
                   choices = list("UW Memes" = "uwMemes", "UC Memes" = "ucMemes")),
-      sliderInput("memeReactionSlide", label = ("Range of X Values:"), min = 0, 
-                  max = 3000, value = 800),
+      sliderInput("memeReactionSlide", label = ("Range of Y Values:"), min = 0, 
+                  max = 12000, value = c(300, 800)),
       selectInput("memeXvar", label = ("Select X Variable:"), 
                   choices = list("Date" = 'status_published', "Number of Reactions" = 'num_reactions', "Number of Comments" = 'num_comments',
                                  "Number of Shares" = 'num_shares', "Number of Likes" = 'num_likes', "Number of Loves" = 'num_loves', "Number of Wows" = 'num_wows',
