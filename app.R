6e5fbfad1340c98904fb06490c42e46d0a77222b
andyblueyo <abchen@uw.edu>
2017-12-08 19:22:19 -0800

    add mvp

--- app.R
+++ app.R
@@ -1,16 +1,42 @@
 ## app.R ##
+library(plotly)
+library(dplyr)
+library(shiny)
+
+uwMemes <- read.csv("data/uwmemesboundlessteens_facebook_statuses.csv", stringsAsFactors = FALSE)
+ucMemes <- read.csv("data/ucberkeleymemes_facebook_statuses.csv", stringsAsFactors = FALSE)
+ucMemes <- head(ucMemes)
+
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+
+  output$memePlot <- renderPlotly({
+    
+    plot_ly(get(input$memeFile), x = ~get(input$memeXvar), y = ~get(input$memeYvar), type = "scatter", mode = "markers")
   })
+
+
 }
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("obs", "Number of observations:", min = 10, max = 500, value = 100)
+      selectInput("memeFile", label = ("Select facebook meme group"), 
+                  choices = list("UW Memes" = "uwMemes", "UC Memes" = "ucMemes")),
+      sliderInput("memeReactionSlide", label = ("Range of X Values:"), min = 0, 
+                  max = 3000, value = 80),
+      selectInput("memeXvar", label = ("Select X Variable:"), 
+                  choices = list("Date" = 'status_published', "Number of Reactions" = 'num_reactions', "Number of Comments" = 'num_comments',
+                                 "Number of Shares" = 'num_shares', "Number of Likes" = 'num_likes', "Number of Loves" = 'num_loves', "Number of Wows" = 'num_wows',
+                                 "Number of Hahas" = 'num_hahas', "Number of Sads" = 'num_sads', "Number of Angrys" = 'num_angrys'),
+                  selected = "status_published"),
+      selectInput("memeYvar", label = ("Select Y Variable:"), 
+                  choices = list("Number of Reactions" = 'num_reactions', "Number of Comments" = 'num_comments',
+                                 "Number of Shares" = 'num_shares', "Number of Likes" = 'num_likes', "Number of Loves" = 'num_loves', "Number of Wows" = 'num_wows',
+                                 "Number of Hahas" = 'num_hahas', "Number of Sads" = 'num_sads', "Number of Angrys" = 'num_angrys'),
+                  selected = "num_reactions")
+
     ),
-    mainPanel(plotOutput("distPlot"))
+    mainPanel(plotlyOutput("memePlot"))
   )
 )
 
